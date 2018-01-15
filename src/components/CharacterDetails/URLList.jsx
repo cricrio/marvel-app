4a646f5bdf3db57b507c099545dc34d6d31b5140
Christopher Bellanger <Christopher.Bellanger.p@gmail.com>
2018-01-15 16:22:20 +0100

    final touch

--- src/components/CharacterDetails/URLList.jsx
+++ src/components/CharacterDetails/URLList.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Link } from 'react-router-dom';
-
 function URL({ name, url }) {
 	return (
 		<a href={url} target="_blank" rel="noopener noreferrer" className="link dim mh2">
